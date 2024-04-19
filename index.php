3a680cb795212ba5d4f0b2d1d87a6b85bb864c19
NguyenBao21012860 <97337580+NguyenBao21012860@users.noreply.github.com>
2024-04-19 08:38:35 +0000

    index.php

--- index.php
+++ index.php
@@ -1,27 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dãy số từ 1 đến 200</title>
+    <style>
+        .so-chan {
+            font-weight: bold;
+            color: red;
+        }
 
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
-?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
+        .so-le {
+            font-style: italic;
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        for ($i = 1; $i <= 200; $i++) {
+            // Kiểm tra số chẵn lẻ
+            if ($i % 2 == 0) {
+                echo "<span class='so-chan'>$i</span> ";
+            } else {
+                echo "<span class='so-le'>$i</span> ";
+            }
+        }
+    ?>
+</body>
 </html>
